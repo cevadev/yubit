cb85a508d19244dec0ec2844dd37410065cf2a69
cevadev <carlos.villanueva.altuna@gmail.com>
2024-04-17 23:00:28 -0300

    Yubit eCommerce -  first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ImgComponent } from './components/img/img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ImgComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrl: './img.component.css'
+})
+export class ImgComponent {
+  img: string = "Valor inicial";
+}
